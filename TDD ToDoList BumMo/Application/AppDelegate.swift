9bc60cf7670a863a77c0a4b31c5058133a4dbc2f
BumMo Koo <ksquareatm@gmail.com>
2019-04-22 14:02:24 +0900

    Initial commit

---
+++ TDD ToDoList BumMo/Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  TDD ToDoList BumMo
+//
+//  Created by BumMo Koo on 22/04/2019.
+//  Copyright © 2019 gbmksquare. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
---
+++ TDD ToDoList BumMo/View Controller/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  TDD ToDoList BumMo
+//
+//  Created by BumMo Koo on 22/04/2019.
+//  Copyright © 2019 gbmksquare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
